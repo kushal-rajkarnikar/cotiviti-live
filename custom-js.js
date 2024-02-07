91692b11cb2907feffbc953a8744445da1805106
kushal-rajkarnikar <kushal.rajkarnikar@gmail.com>
2024-02-07 19:36:20 +0545

    Initial push

---
+++ custom-js.js
@@ -0,0 +1,36 @@
+var fullname = "Ram";
+console.log("Your name is " + fullname);
+
+// Arithmetic Operations
+// Addition
+// Subtraction
+// Multiplication
+// Division
+// Modulo / Modulos Devision
+
+var num1 = 89;
+var num2 = 66;
+var sum = num1 + num2;
+var sub = num1 - num2;
+var product = num1 * num2;
+var divide = num1 / num2;
+var rem = num1 % num2;
+console.log("Sum of 2 number is " + sum);
+console.log("Difference of 2 number is " + sub);
+console.log("Product of 2 number is " + product);
+console.log("division of 2 number is " + divide);
+console.log("Remainder of 2 number is " + rem);
+
+//display notice or information to user
+// alert("Hello World!");
+// confirm("Are you sure you want to continue?");
+
+var name = parseInt(prompt("Enter your name"));
+alert("you entered " + name);
+console.log("You entered " + name);
+// Datatype
+// String, int, float, boolean
+
+// user input 2 input
+// 5 operations
+// alert output
